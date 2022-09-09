e3819d27a9d696289f9da9b0652934422942d0ea
Enrico Serrano <enricoserrano13@gmail.com>
2022-09-09 17:36:48 +1200

    Fixed heading, subheading and text field design
    -Design and margin fixes

--- passwordstrength-app/src/App.js
+++ passwordstrength-app/src/App.js
@@ -5,10 +5,12 @@ import { MDBInput } from 'mdb-react-ui-kit';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Simple Password Strength Checker</h1>
-      <h6>Developed by Enrico Serrano</h6>
+    <div className="Container">
+      <div className="col-md-4 mx-auto">
+      <h1 className="text-center">Simple Password Strength Checker</h1>
+      <h6 className="text-center">Developed by Enrico Serrano</h6>
       <MDBInput label='Enter Desired Password' id='form1' type='text' />
+    </div>
     </div>
   );
 }
