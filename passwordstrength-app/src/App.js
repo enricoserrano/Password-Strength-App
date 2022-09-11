c31ea628d9060056f152867fed627ccb3ba66966
Enrico Serrano <enricoserrano13@gmail.com>
2022-09-11 20:42:54 +1200

    Created first functional prototype of the app
    -Prototype v1

--- passwordstrength-app/src/App.js
+++ passwordstrength-app/src/App.js
@@ -1,15 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { MDBInput } from 'mdb-react-ui-kit';
+import PasswordStrength from './components/PasswordStrength';
 
 function App() {
+
+const [ password, setPassword] = useState('');
+
   return (
     <div className="Container">
       <div className="col-md-4 mx-auto">
       <h1 className="text-center">Simple Password Strength Checker</h1>
-      <h6 className="text-center">Developed by Enrico Serrano</h6>
-      <MDBInput label='Enter Desired Password' id='form1' type='text' />
+      <h4 className="text-center">Developed by Enrico Serrano</h4>
+      <p className="text-center">This application uses 'zxcvbn' realistic password strength estimation <a href="https://yarnpkg.com/package/zxcvbn" target="_blank">here</a></p>
+      <p>In order for a password to be considered secure it must meet all of these requirements:</p>
+      <ul>
+        <li>At least <b>9 characters</b></li>
+        <li>At least <b>1 uppercase letter</b></li>
+        <li>At least <b>1 lowercase letter</b></li>
+        <li>At least <b>1 number</b></li>
+        <li>At least <b>1 special character</b></li>
+      </ul>
+      <div className="form-group mb-1">
+      <MDBInput label='Enter Desired Password' id='form1' type='password' onChange={e => setPassword(e.target.value)}/>
+      </div>
+      <PasswordStrength password={password}></PasswordStrength>
     </div>
     </div>
   );
---
+++ passwordstrength-app/src/components/PasswordStrength.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import zxcvbn from 'zxcvbn'
+
+const PasswordStrength = ({ password }) => {
+
+    const testResult = zxcvbn(password);
+    const num = testResult.score * 100/4;
+    const passwordlabel = () => {
+        switch(testResult.score) {
+            case 0:
+                return 'Very Weak';
+            case 1:
+                return 'Weak';
+            case 2:
+                return 'Fair';
+            case 3:
+                return 'Good';
+            case 4:
+                return 'Strong';
+            default:
+                return '';
+
+        }
+    }
+    const ProgressColor = () => {
+        switch(testResult.score) {
+            case 0:
+                return '#828282';
+            case 1:
+                return '#EA1111';
+            case 2:
+                return '#FFAD00';
+            case 3:
+                return '#9bc158';
+            case 4:
+                return '#00b500';
+            default:
+                return 'none';
+
+        }
+    }
+
+    const changePasswordColor = () => ({
+        width: `${num}%`,
+        background: ProgressColor(),
+        height: '7px'
+    })
+  return (
+    <>
+    <div className="progress" style= {{ height: '7px' }}>
+        <div className="progress-bar" style={changePasswordColor()}></div>
+    </div>
+    <p style={{ color: ProgressColor() }}>{passwordlabel()}</p>
+    </>
+  )
+}
+
+export default PasswordStrength
